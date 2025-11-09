bdf3fe2e81efb0e7b52ba7e64e4ac5e4efb52510
Abhishek Deep <dpabhishek321@gmail.com>
2025-11-09 21:09:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+﻿let fighters = ['🐉', '🐊', '🦁', '🦍', '🐘', '🐩', '🐆', '🦀', '🦭',
+                        '🐥', '🐝', '🐢', '🐸', '🕷️', '🤖', '💩', '🦕']
+
+let stageEl = document.getElementById('stage-el');
+
+function startBattle() {
+    let playerA = Math.floor(Math.random()*fighters.length);
+    let playerB = Math.floor(Math.random()*fighters.length);
+    stageEl.textContent = fighters[playerA] + '     V/S      ' + fighters[playerB];
+}
+
+let fruits = ['🍎', '🍊', '🍎', '🍎', '🍊']
+let orangeEl = document.getElementById('orange-el')
+let appleEl = document.getElementById('apple-el')
+let appleShelf = []
+let orangeShelf = []
+
+function sortFruits() {
+    for (let i = 0; i < fruits.length; i++) {
+        if (fruits[i] === '🍎' && appleShelf.length < 4 ) {
+            appleShelf.push(fruits[i]);
+        } else if (orangeShelf.length < 3 ) {
+            orangeShelf.push(fruits[i]);
+        } else {
+            return ''
+        }
+    }
+    appleEl.textContent = appleShelf.join(' ');
+    orangeEl.textContent = orangeShelf.join(' ');
+}
+
+function resetFruits() {
+    appleEl.textContent = '🍎 🍊 🍎'
+    orangeEl.textContent = '🍊 🍎'
+    appleShelf = []
+    orangeShelf = []
+}
